142da4c08f4ab3a980cadb2950d31924fd5b3b44
IT-kubat < gmail adresskubat88@list.ru>
2019-12-25 17:49:14 +0500

    Mistake is done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,58 +7,45 @@ public class Main {
     public static void main(String[] args) {
 
         HashMap<String, String[]> dictionary = new HashMap<>();
-        dictionary.put("Cool",
+        dictionary.put("cool",
                 new String[]{"beautiful", "wonderful", "smart", "calm"});
-        dictionary.put("House",
+        dictionary.put("house",
                 new String[]{"Zhilishe", "Hizhina", "Department", "Kvartiry"});
-        dictionary.put("Chelovek",
+        dictionary.put("chelovek",
                 new String[]{"Lichnost'", "Persona", "Homosapiens", "Individum"});
+
         HashMap<String, String[]> bigDictionary = new HashMap<>();
 
         Set<String> keys = dictionary.keySet();
-        Iterator iter = keys.iterator();
-        while (iter.hasNext()) {
-            String key = iter.next().toString();
-            String[] value = dictionary.get(key);
-            bigDictionary.put(key, value);
-            for (int i = 0; i < value.length; i++) {
-                String newKey = value[i];
-                ArrayList<String> arrayList = new ArrayList<>();
-                arrayList.addAll(Arrays.asList(value));
-                arrayList.remove(newKey);
-                arrayList.add(key);
-                String[] newValue = new String[arrayList.size()];
-                newValue = arrayList.toArray(newValue);
-                bigDictionary.put(newKey, newValue);
+        Iterator iterator = keys.iterator();
+        while (iterator.hasNext()){
+            String key = iterator.next().toString();
+            String[] values =dictionary.get(key);
+            bigDictionary.put(key,values);
+            for (int i = 0; i <values.length ; i++) {
+                String new_Key = values[i];
+                ArrayList<String> stringArray = new ArrayList<>();
+                stringArray.addAll(Arrays.asList(values));
+                stringArray.remove(new_Key);
+                stringArray.add(key);
+                String[] newVal = new String[stringArray.size()];
+                newVal = stringArray.toArray(newVal);
+                bigDictionary.put(new_Key, newVal);
 
             }
         }
-//            System.out.println("Введите любое слово из словаря: ");
-//    Scanner s = new Scanner(System.in);
-//    String sentences = s.nextLine().toLowerCase();
-//    String[] words = sentences.split(" ");
-//    Random r = new Random();
-//        for (int i = 0; i < words.length; i++) {
-//        String[] synonyms = bigDictionary.get(words[i]);
-//        if (synonyms != null) {
-//            int randomNumber = r.nextInt(synonyms.length);
-//            System.out.printf(synonyms[randomNumber] + " ");
-//        } else {
-//            System.out.println("Не понял Вас");
-//        }
         System.out.println("Enter your word");
         Scanner scan = new Scanner(System.in);
         String sentence = scan.nextLine();
         String[] words = sentence.split(" ");
         Random r = new Random();
         for (String word : words) {
-            String[] synonyms = dictionary.get(word);
+            String[] synonyms = bigDictionary.get(word);
             if (synonyms != null) {
                 System.out.printf(synonyms[r.nextInt(synonyms.length)] + " ");
             } else {
                 System.out.println("Такого слова в словаре нет");
             }
-
         }
     }
 }
